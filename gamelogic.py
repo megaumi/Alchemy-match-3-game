7636adb154e11c016cc9e180721e72ee88489d41
Valentina Mukhamedzhanova <umirra@gmail.com>
2011-01-09 14:31:32 +0300

    added coords check

--- gamelogic.py
+++ gamelogic.py
@@ -7,30 +7,27 @@ class Game(object):
     def __init__(self, grid_size = 10):
         self.grid = [["0" for x in range(grid_size)] for y in range(grid_size)]
         self.colors = ["■", "□", "◆"]
-        
         self.figure_max_size = 4
         
     def run(self):
         '''Game cycle'''
         while True:
+            # Generate a new figure
             self.next_figure = self.get_next_figure()
-            row, col = self.get_user_input()
-
-            # Put every cell of the figure on the grid
-            for rnum, c_row in enumerate(self.next_figure):
-                for cnum, c_col in enumerate(c_row):
-                    if c_col: self.grid[row+rnum][col+cnum] = c_col
+            
+            # Let the user rotate and place the figure
+            row, col = self.place_figure()
             
             # Find and destroy matches for every cell of the figure
             for rnum, c_row in enumerate(self.next_figure):
-                for cnum, c_col in enumerate(c_row):
-                    if c_col: self.handle_matches(row+rnum, col+cnum, c_col)
+                for cnum, cell in enumerate(c_row):
+                    if cell: self.handle_matches(row + rnum, col + cnum, cell)
             
             # Show the grid
             for row in self.grid: print " ".join(row)
             
     def get_next_figure(self):
-        '''Generate new figure and show it to user'''
+        '''Generate a new figure and show it to the user'''
         
         # Based on size of the figure and number of rows get number of cols and empty cells.
         # FIXME: Sometimes this algorithm generates wrong polyominoes (with non-adjacent cells)
@@ -51,42 +48,67 @@ class Game(object):
         
         next_figure = [["" for col in range(cols)] for row in range(rows)]
         
-        #print size, rows, cols, empty
-        
-        # Generate colors (including empties) for the next figure and store them in a temporary array
+        # Generate cells (including empties) for the next figure and store them in a temporary array
         temp_arr = [random.choice(self.colors) for i in range(size)]
         temp_arr.extend([""] * empty)
         
         # Generate the next figure
         for row in range(rows):
             for col in range(cols):
-                cell_color = random.choice(temp_arr)
-                temp_arr.remove(cell_color)
-                next_figure[row][col] = cell_color
+                cell = random.choice(temp_arr)
+                temp_arr.remove(cell)
+                next_figure[row][col] = cell
         
         #next_figure = [(row, col, temp_arr.pop(random.randrange(0,len(temp_arr)))) for col in range(cols) for row in range(rows)]
                 
         # Represent the next figure in nice human-readable form
         print "Figure:"
-        repr_figure = [[col if col else " " for col in row] for row in next_figure]
+        repr_figure = [[cell if cell else " " for cell in row] for row in next_figure]
         for row in repr_figure: print " ".join(row)
         
         return next_figure
         
-    def get_user_input(self):
+    def place_figure(self):
+        '''Check if the figure can be placed on the grid using rotation and coords that user provided.
+           In case of success, place the figure to the grid and return its coords.'''
         
         rotate = int(raw_input("\nRotate the figure (0 - do not rotate, 1 - rotate 90 clockwise, 2 - 180, etc): "))
         
         for i in range(rotate):
             self.next_figure = zip(*self.next_figure[::-1])
         
-        row, col = [int(var) for var in raw_input("\nInput coords: ")]
+        # Check if every cell of the figure can be placed on the grid using the coordinates
+        # that user provided. If any of the cells cannot be placed (the place is already taken or 
+        # it's outside of the grid), user must provide new coords.
+        coords_checked = False
+        while not coords_checked:
+            row, col = [int(var) for var in raw_input("\nInput coordinates: ")]
+            check_results = []
+            for rnum, c_row in enumerate(self.next_figure):
+                for cnum, cell in enumerate(c_row):
+                    if not cell: continue       # If the cell is '' it can be placed anywhere
+                    try:
+                        if self.grid[row + rnum][col + cnum] == "0":
+                            check_results.append(True)
+                        else:
+                            print "Oops! You are trying to place %s above %s in (%i, %i)!" %(cell, self.grid[row+rnum][col+cnum], row+rnum, col+cnum)
+                            check_results.append(False)
+                    except IndexError: 
+                        print "Oops! You are trying to place %s to (%i, %i) which is outside of the grid!" %(cell, row+rnum, col+cnum)
+                        check_results.append(False)
+            coords_checked = all(check_results)
+        
+        # Put every cell of the figure on the grid
+        for rnum, c_row in enumerate(self.next_figure):
+            for cnum, cell in enumerate(c_row):
+                if cell: self.grid[row + rnum][col + cnum] = cell
+        
         return row, col
         
-    def handle_matches(self, row, col, cell_color):
+    def handle_matches(self, row, col, cell):
         '''Find all matches with the current cell and destroy all matching cells'''
         
-        def check_direction(row, col, dir, cell_color, counter, to_del_coords):
+        def check_direction(row, col, dir, cell, counter, to_del_coords):
             '''Check the next cell in given direction. If it has the same color as the current cell,
             store its coords (to delete it later) and pass it to this function recursively
             with incremented counter'''
@@ -97,11 +119,11 @@ class Game(object):
             # We don't want to get outside of our grid
             if not (n_row < 0 or n_col < 0 or n_row >= len(self.grid) or n_col >= len(self.grid)):
                 #print "I'm in (%i, %i), counter = %i, checking (%i, %i)..." %(row, col, counter, n_row, n_col)
-                if self.grid[n_row][n_col] == cell_color:
+                if self.grid[n_row][n_col] == cell:
                     to_del_coords.append([n_row, n_col])
                     counter += 1
                     #print "Found a cell of the same color, incrementing counter..."
-                    counter, to_del_coords = check_direction(n_row, n_col, dir, cell_color, counter, to_del_coords)
+                    counter, to_del_coords = check_direction(n_row, n_col, dir, cell, counter, to_del_coords)
             
             return counter, to_del_coords
 
@@ -116,8 +138,8 @@ class Game(object):
         counter = 0
         to_del_coords = []
        
-        counter, to_del_coords = check_direction(row, col, up, cell_color, counter, to_del_coords)
-        counter, to_del_coords  = check_direction(row, col, down, cell_color, counter, to_del_coords)
+        counter, to_del_coords = check_direction(row, col, up, cell, counter, to_del_coords)
+        counter, to_del_coords  = check_direction(row, col, down, cell, counter, to_del_coords)
 
         if counter >= 2: 
             match_found = True
@@ -127,8 +149,8 @@ class Game(object):
         counter = 0
         to_del_coords = []
 
-        counter, to_del_coords = check_direction(row, col, left, cell_color, counter, to_del_coords)
-        counter, to_del_coords = check_direction(row, col, right, cell_color, counter, to_del_coords)
+        counter, to_del_coords = check_direction(row, col, left, cell, counter, to_del_coords)
+        counter, to_del_coords = check_direction(row, col, right, cell, counter, to_del_coords)
 
         if counter >= 2: 
             match_found = True
